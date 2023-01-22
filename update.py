9e816617c708853ff94e9e93a3c2be6d08952f16
gfdgd-xi <3025613752@qq.com>
2023-01-22 16:09:18 +0800

    a

--- update.py
+++ update.py
@@ -15,9 +15,9 @@ html = f"""<!DOCTYPE html>
     <h1>Wine 容器列表（来自 Wine 运行器）</h1>
     <h3>更新时间：{datetime.datetime.now().year}年{datetime.datetime.now().month}月{datetime.datetime.now().day}日 {datetime.datetime.now().hour}:{datetime.datetime.now().minute}:{datetime.datetime.now().second}</h3>
 """
-lists = requests.get("https://code.gitlink.org.cn/gfdgd_xi/wine-mirrors/raw/branch/master/information.json").json()
+lists = requests.get("https://code.gitlink.org.cn/gfdgd_xi/wine-bottle-library/raw/branch/master/information.json").json()
 for i in lists:
-    html += f"    <p><a href='https://code.gitlink.org.cn/gfdgd_xi/wine-mirrors/raw/branch/master/{i[1]}'>{i[0]}</a></p>\n"
+    html += f"    <p><a href='https://code.gitlink.org.cn/gfdgd_xi/wine-bottle-library/raw/branch/master/{i[1]}'>{i[0]}</a></p>\n"
 html += f"""    <p><a href='https://gitee.com/gfdgd-xi-org/deep-wine-runner/stargazers'><img src='https://gitee.com/gfdgd-xi-org/deep-wine-runner/badge/star.svg?theme=dark' alt='star'></img></a><a href='https://gitee.com/gfdgd-xi-org/deep-wine-runner/members'><img src='https://gitee.com/gfdgd-xi-org/deep-wine-runner/badge/fork.svg?theme=dark' alt='fork'></img></a></p>
     <hr/>
     <h2>Wine 运行器</h2>
